3181af55bda2704916077611ab5d01864b5f4052
[shayan ahmad] <ahmadshayan200429@gmail.com>
2024-10-01 03:16:00 +0530

    Intialization of selenium project

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+  
+public class App {  
+  
+    public static void main(String[] args) {  
+        
+    WebDriver driver=new ChromeDriver();  
+      
+// Launch website  
+    driver.navigate().to("https://github.com/shayan729");  
+          
+    
+      
+    }  
+  
+}  
